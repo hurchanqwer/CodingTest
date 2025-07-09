5d2a5819cc01c11d967b4c023ae666854b20510c
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2025-07-09 16:15:15 +0900

    [level 2] Title: 프로세스, Time: 2.42 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cs
+++ 프로그래머스/2/42587. 프로세스/프로세스.cs
@@ -1,9 +1,31 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int solution(int[] priorities, int location) {
+  
+        Queue<(int priority , int index)> waitQ = new  Queue<(int, int)>();
         int answer = 1;
+        int index=0;
         
+        foreach (int p in priorities)
+            waitQ.Enqueue((p, index++));
+        
+        while(waitQ.Count > 0){
+            var cur = waitQ.Dequeue();
+            if(waitQ.Any(x => x.priority > cur.priority)){
+                waitQ.Enqueue(cur);
+            }
+            else{
+                if (cur.index == location) break;
+                answer++;
+            }
+        }
+        
+        return answer;
+    
+        //큐 안쓰고 더 빠름
+        /*int answer = 1;
         int[] process = (int[])priorities.Clone();
         Array.Sort(priorities);
         Array.Reverse(priorities);
@@ -12,19 +34,15 @@ public class Solution {
         
         while(priorities.Length != 0){
             for(int i =0; i<process.Length; i++){
-              
                 if(process[i] != max)
                   continue;
-                
-                    if(i==location)
-                        return answer;
-                  
-                    max = priorities[index++];
-                    answer++;
-                
+                if(i==location)
+                  return answer;
+                max = priorities[index++];
+                answer++;
             }
        }
         
-        return answer;
+        return answer;*/
     }
 }
