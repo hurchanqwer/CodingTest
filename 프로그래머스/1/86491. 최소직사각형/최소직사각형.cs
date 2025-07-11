bd61e19e06d6cf3348eff10731895cbd62386106
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2025-07-11 15:39:17 +0900

    [level 1] Title: 최소직사각형, Time: 0.56 ms, Memory: 32 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+public class Solution
+{
+    public int solution(int[,] sizes)
+    {
+
+        int w = 0, h = 0;
+        for (int i = 0; i < sizes.GetLength(0); i++)
+        {
+            if (sizes[i, 0] > sizes[i, 1])
+            {
+                w = sizes[i, 0] > w ? sizes[i, 0] : w;
+                h = sizes[i, 1] > h ? sizes[i, 1] : h;
+            }
+            else
+            {
+                w = sizes[i, 1] > w ? sizes[i, 1] : w;
+                h = sizes[i, 0] > h ? sizes[i, 0] : h;
+            }
+        }
+  
+        return w*h;
+    }
+}
