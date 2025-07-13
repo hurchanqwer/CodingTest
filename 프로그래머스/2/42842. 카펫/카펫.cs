5d5a26792e6d7a29ed5e2c9d7181dade617aa42e
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2025-07-14 00:37:14 +0900

    [level 2] Title: 카펫, Time: 0.19 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int[] solution(int brown, int yellow) {
+        int total = brown + yellow;
+
+        for (int height = 3; height <= total / 3; height++) {
+            if (total % height != 0) continue;
+            int width = total / height;
+
+            if ((width - 2) * (height - 2) == yellow) {
+                return new int[] { width, height };
+            }
+        }
+
+        return null;
+    }
+}
