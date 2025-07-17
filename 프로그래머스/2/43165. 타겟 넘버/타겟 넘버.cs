3875350e18c1c4e24f45778cb20e5faf79163aa1
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2025-07-17 16:44:04 +0900

    [level 2] Title: 타겟 넘버, Time: 5.98 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cs
@@ -1,28 +1,21 @@
 using System;
 
-public class Solution
-{
-    int answer = 0;
-    public int solution(int[] numbers, int target)
-    {
-        //- + 이진 트리
-        //시작부터 끝까지 탐색
-        //타겟 값인지 체크
-        //맞으면 answer++;
-        DFS(0, 0, numbers, target);
+public class Solution {
+     int answer;
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+
+        dfs(0,0,numbers,target);
         return answer;
     }
-    public void DFS(int depth, int sum, int[] numbers, int target)
-    {
-     
-        if (depth == numbers.Length)
-        {
-            if (sum == target)
+    void dfs(int depth, int sum, int[] numbers, int target){
+        if(depth == numbers.Length){
+            
+            if(sum == target)
                 answer++;
             return;
         }
-        DFS(depth+1, sum+numbers[depth], numbers, target);
-        DFS(depth+1, sum- numbers[depth], numbers, target);
+        dfs(depth+1, sum + numbers[depth],numbers,target);
+        dfs(depth+1, sum - numbers[depth],numbers,target);
     }
-
 }
