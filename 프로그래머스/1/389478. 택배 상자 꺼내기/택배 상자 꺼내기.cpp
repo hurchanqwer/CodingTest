ffaff2eba82f01b1d7106d533eada5114de2004f
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2025-08-25 02:39:47 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.cpp
@@ -1,41 +1,24 @@
-#include <string>
-#include <vector>
-#include <iostream>
-using namespace std;
-
 int solution(int n, int w, int num)
 {
     int answer = 0;
 
-    // 현재 층
-    int floor = (num - 1) / w + 1;
-    
-    // 현재 열
-    int pos = (num - 1) % w + 1;
-
-    // 방향 계산
-    int col = (floor % 2 == 1) ? pos : w - pos + 1;
+    int floor = (num - 1) / w + 1;          // 층
+    int pos = (num - 1) % w + 1;            // 기본 열
+    int col = (floor % 2 == 1) ? pos : w - pos + 1; // 실제 열 번호
 
     int boxNum = num;
 
     while (boxNum <= n)
     {
         answer++;
-
         floor++;
 
-        if (floor > (n + w - 1) / w)
-            break; // 층 넘어가면 종료
+        if (floor > (n + w - 1) / w) break; // 창고 높이 넘으면 종료
 
-        if (floor % 2 == 1) // 홀수 층
-        {
+        if (floor % 2 == 1) // 홀수층
             boxNum = (floor - 1) * w + col;
-        }
-        else // 짝수 층
-        {
+        else // 짝수층
             boxNum = floor * w - col + 1;
-        }
     }
-
     return answer;
 }
