29b55a14f0f18a2dfa099a94d0a68118c435ff00
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2025-08-28 05:10:03 +0900

    [level 2] Title: 프로세스, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    priority_queue<int> waitQ;
+
+    int index = 0;
+    
+    for(int p : priorities){
+        waitQ.push(p);
+    }
+
+    while(waitQ.size()>0)
+    {
+        for(int i= 0; i<priorities.size();i++){
+            if(waitQ.top() == priorities[i])
+            {
+                answer++;
+                waitQ.pop();
+                
+                if(i == location){
+                    return answer;
+                }
+            }
+        }
+    }    
+    
+    return answer;
+}
