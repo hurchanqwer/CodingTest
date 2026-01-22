937790573a39bd0d90af24711446d488826e651e
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2026-01-22 23:46:44 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.18 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.cs
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.cs
@@ -2,9 +2,6 @@ using System;
 
 public class Solution {
     public bool solution(bool x1, bool x2, bool x3, bool x4) {
-     
-        
-  
-        return      ( x1 || x2) &&( x3 || x4);
+        return  ( x1 || x2) &&( x3 || x4);
     }
 }
