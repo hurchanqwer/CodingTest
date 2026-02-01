db8f3f061416d1293653039d14705f24ef7c6c60
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2026-02-02 00:15:09 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.96 ms, Memory: 32 MB -BaekjoonHub

--- 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.cs
@@ -3,15 +3,6 @@ using System;
 public class Solution {
     public string solution(string my_string, int s, int e) {
         string answer = "";
-        for(int i=0; i< s; i++)
-            answer += my_string[i];
-        
-        for(int i=e; i >= s; i--)
-            answer += my_string[i];
-        
-        for(int i=e+1; i < my_string.Length; i++)
-            answer += my_string[i];
-        
         return answer;
     }
 }
