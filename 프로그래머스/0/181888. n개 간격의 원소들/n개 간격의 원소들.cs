8ae8b092518346d1dbd542c01e3db6eee2435489
hurchanqwer <64076149+hurchanqwer@users.noreply.github.com>
2026-02-05 18:42:45 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.83 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+public class Solution {
+    public int[] solution(int[] num_list, int n) {
+        List<int> answer = new List<int>();
+        int index=0;
+        for(int i =0; i< num_list.Length;i+=n)
+            answer.Add(num_list[i]);
+        return answer.ToArray();
+    }
+}
